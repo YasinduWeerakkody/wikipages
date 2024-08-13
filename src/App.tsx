297d3893029f3df41a91d6e68fa2fb6ce172824a
YasinduWeerakkody <yasiweera1234@gmail.com>
2024-08-13 23:56:06 +0530

    add
    wiki pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import Board from './components/board';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import IykonsArticle from './components/IykonsArticle';
+import Article_Group from './components/Article_Group';
+import NewsCardList from './components/card';
+import Article from './components/Article';
 
 function App() {
   return (
     <div className="App">
-      <Board/>
+      {/* <Board/> */}
       {/* <Dashboard/> */}
+      {/* <IykonsArticle/> */}
+      {/* <Article_Group/> */}
+      <Article/>
+      {/* <NewsCardList/> */}
     </div>
   );
 }
